9e582da8e481e7ceb36055d7be982982a3d691e8
ftmamhmod <ftmamhmod2@gmail.com>
2025-07-24 18:20:47 +0300

    add : loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./combonents/Footer/Footer";
 import Header from "./combonents/Header/Header";
@@ -6,9 +7,48 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About/About";
 import Menu from "./pages/Menu/Menu";
 import Contact from "./pages/Contact/Contact";
+import loading from "./assets/images/loading-logo-bazooka.gif";
 
-export default App;
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 9999,
+        }}
+      >
+        <img
+          src={loading}
+          alt="Loading..."
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            objectPosition: "center",
+          }}
+        />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <Header />
@@ -23,3 +63,5 @@ function App() {
     </Router>
   );
 }
+
+export default App;
